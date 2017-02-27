02e7f3f9765a12e3c0c5c78863cd47c90a275942
mcmurdo1471 <1471@mcmurdo>
2017-02-27 21:18:52 +0000

    Update to POINTER

--- vmtranslator/VmTranslator/CodeWriter.pm
+++ vmtranslator/VmTranslator/CodeWriter.pm
@@ -67,6 +67,7 @@ given it is passed to C<< $hello->target >>.
 	print $fh "// this segment\n\@3000\nD=A\n\@THIS\nM=D\n";
 	print $fh "// that segment\n\@3010\nD=A\n\@THAT\nM=D\n";
 	print $fh "// temp segment\n\@5\nD=A\n\@TEMP\nM=D\n";
+	print $fh "// pointer segment\n\@3\nD=A\n\@POINTER\nM=D\n";
 	#print $fh "// register segment\n\@13\nD=A\n\@REG\nM=D\n";
 	
 	# Set initial lablecounter to 0
@@ -161,25 +162,27 @@ sub writePushPop {
 			print $fh $instructions->{"pushresult"};
 			return;
 		}
-		else {
-			print $fh "\@$index\nD=A\n";
+				
+		print $fh "\@$index\nD=A\n";
 		
-			print $fh "\@LCL\n" if($segment eq "local");
-			print $fh "\@ARG\n" if($segment eq "argument");
-			print $fh "\@THIS\n" if($segment eq "this");
-			print $fh "\@THAT\n" if($segment eq "that");
-			print $fh "\@TEMP\n" if($segment eq "temp");
+		print $fh "\@LCL\n" if($segment eq "local");
+		print $fh "\@ARG\n" if($segment eq "argument");
+		print $fh "\@THIS\n" if($segment eq "this");
+		print $fh "\@THAT\n" if($segment eq "that");
+		print $fh "\@TEMP\n" if($segment eq "temp");
+		print $fh "\@POINTER\n" if($segment eq "pointer");
 			
-			print $fh "D=D+M\n\@mem\nM=D\n"; # location of segment+index in @mem
+		print $fh "D=D+M\n\@mem\nM=D\n"; # location of segment+index in @mem
 			
-			print $fh "\@mem\nA=M\nD=M\n";
-			print $fh $instructions->{"pushresult"};
+		print $fh "\@mem\nA=M\nD=M\n";
+		print $fh $instructions->{"pushresult"};
 			
-			return;
-		}
+		return;
+		
 	}
 	
 	if($command eq "C_POP") {
+	
 		print $fh $instructions->{"popintovar1"};
 		# Pop from stack into segment
 		print $fh "\@$index\nD=A\n";
@@ -189,8 +192,9 @@ sub writePushPop {
 		print $fh "\@THIS\n" if($segment eq "this");
 		print $fh "\@THAT\n" if($segment eq "that");
 		print $fh "\@TEMP\n" if($segment eq "temp");
+		print $fh "\@POINTER\n" if($segment eq "pointer");
 		#TODO Bounds checking!
-		print $fh "// here\n";
+		
 		print $fh "D=D+M\n\@mem\nM=D\n\@var1\nD=M\n\@mem\nA=M\nM=D\n";
 		return;
 	}
